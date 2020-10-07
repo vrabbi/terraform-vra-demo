8677b7942d023fe6d20ca26b25cfc81cdfa0cb95
Scott Rosenberg <48493016+vrabbi@users.noreply.github.com>
2020-10-07 16:19:19 +0300

    Update 01-cluster.tf

--- capv-cluster/01-cluster.tf
+++ capv-cluster/01-cluster.tf
@@ -30,7 +30,7 @@ resource "kubernetes_manifest" "cluster" {
   }
   wait_for = {
     fields = {
-      "status.conditions.0.status" = "True"
+      "Status.Conditions[0].Status" = "True"
     }
   }
 }
